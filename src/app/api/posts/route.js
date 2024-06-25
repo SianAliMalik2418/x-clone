7429bd5ae7064ba195b59aab50eca93e9330c417
SianAliMalik2418 <sianmalik2418@gmail.com>
2024-06-25 09:06:04 +0500

    upload to cloudinary completed

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -3,6 +3,21 @@ import path from "path";
 import fs from "fs/promises";
 import { v4 as uuidv4 } from "uuid";
 import os from "os";
+import cloudinary from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadImagesToCloudinary = async (newFiles) => {
+  const multiplePhotosPromise = newFiles.map((file) =>
+    cloudinary.v2.uploader.upload(file.filePath, { folder: "nextjs-x-clone" }),
+  );
+
+  return await Promise.all(multiplePhotosPromise);
+};
 
 const storeImagesTemporarily = async (files) => {
   const multipleBufferPromise = files.map((file) =>
@@ -38,8 +53,13 @@ export const POST = async (request) => {
 
     // Saving files in temp folder.
     const newFiles = await storeImagesTemporarily(files);
-    console.log(newFiles);
 
+    // Returns the url
+    const photos = await uploadImagesToCloudinary(newFiles);
+    console.log(photos);
+
+    // Delete the images from temp after sucessful upload.
+    newFiles.map((file) => fs.unlink(file.filePath));
 
     return NextResponse.json(reqBody);
   } catch (error) {
